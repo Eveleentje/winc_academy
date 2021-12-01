e88418fed699f5b939c8598d89e666974ec83c4f
Eveleentje <79459744+Eveleentje@users.noreply.github.com>
2021-12-01 14:36:50 +0100

    Add files via upload

---
+++ javascript_week_4.1/script.js
@@ -0,0 +1,7 @@
+console.log("Hello Winc Academy students");
+// This is a large sum
+/* let sumOne = 1230941 + 1823794;
+console.log(sumOne);
+let sumTwo = 637263 / 54;
+console.log(sumTwo); */
+// comments are not javascript code so are not visible in the console
