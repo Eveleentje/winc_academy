d29fad5b4eee4d7c03fbaeac0934549667f096d3
Eveleentje <79459744+Eveleentje@users.noreply.github.com>
2021-12-01 14:39:00 +0100

    Add files via upload

--- javascript_week_4.1/script.js
+++ javascript_week_4.1/script.js
@@ -1,7 +1,13 @@
 console.log("Hello Winc Academy students");
 // This is a large sum
+let sumOne = 1230941 + 1823794;
+console.log(sumOne);
+let sumTwo = 637263 / 54;
+console.log(sumTwo);
 /* let sumOne = 1230941 + 1823794;
 console.log(sumOne);
 let sumTwo = 637263 / 54;
 console.log(sumTwo); */
 // comments are not javascript code so are not visible in the console
+// Note: Does JavaScript run in Node? Then your terminal is the console.
+// Does JavaScript run in your browser? Then you will find the console in your developer tools.
