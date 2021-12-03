7c5063b23f14da791c9bc758260a4ca434353256
Eveleentje <79459744+Eveleentje@users.noreply.github.com>
2021-12-03 11:02:22 +0100

    Add files via upload

---
+++ javascript_week_5/script.js
@@ -0,0 +1,25 @@
+//bouncer at the pub
+
+const age = 25;
+if (age >= 18) {
+    console.log("welcome to the pub")
+} 
+else {
+    console.log("you are too young, please do not enter")
+}
+
+const isFemale = true;
+if (isFemale == true) {
+    console.log("welcome to ladies night")
+}
+else {
+    console.log("sorry, this night is ladies only. we hope to see you back some other day")
+}
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("you are allowed to drive")
+}
+else {
+    console.log("you cannot drive")
+}
