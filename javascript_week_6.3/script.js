322c5c7fa7b070b8e75cda0ce7038bbe38c8838d
Eveleentje <79459744+Eveleentje@users.noreply.github.com>
2021-12-20 11:57:31 +0100

    Add files via upload

--- javascript_week_6.3/script.js
+++ javascript_week_6.3/script.js
@@ -1,20 +1,27 @@
-
-const paint = function() {
-    console.log("The " + color + " wall has been painted " + face);
+const paintWall1 = function() {
+    console.log("The wall has been painted red");
 };
 
-const face = "north";
-const color = "green";
+paintWall1();
 
-paint();
+const paintWall2 = function(color) {
+    console.log("The wall has been painted " + color);
+};
 
-// Single argument q 7: calling the function without an argument returns an error
+paintWall2("green");
 
-//  Multiple arguments q 6: the order of the arguments does not matter, 
-//  as long as they're placed before calling the function 
+const paintWall3 = function(face,color) {
+    console.log("The " + face + " wall has been painted " + color);
+};
 
-// Multitple arguments q 7: switching the arguments in the function definition changes 
-// their places in the output too
+paintWall3("north","yellow");
 
-// Multiple arguments q 8: changing the order of the arguments only has effect
-// on the output when it's done in the function definition. Otherwise it does not matter
+// The order of the arguments matters:
+
+paintWall3("purple","south");
+
+const paintWall4 = function(face,color) {
+    console.log("The " + face + " wall has been painted " + color);
+};
+
+paintWall4("northeast","eggshell");
