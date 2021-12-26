aea70760660252a28de4b03aac70833112dccf27
Eveleentje <79459744+Eveleentje@users.noreply.github.com>
2021-12-26 20:41:59 +0100

    Add files via upload

---
+++ javascript_week_7.1/script.js
@@ -0,0 +1,38 @@
+// function declaration
+
+function calculationDeclaration(number1, number2) {
+    squareDeclaration1 = number1 * number1;
+    squareDeclaration2 = number2 * number2;
+    sumDeclaration = squareDeclaration1 + squareDeclaration2;
+
+    return sumSquareDeclaration = sumDeclaration * sumDeclaration;
+};
+
+calculationDeclaration(5, 8);
+console.log(sumSquareDeclaration);
+
+// function expression
+
+const calculationExpression = function (number3, number4) {
+    squareExpression1 = number3 * number3;
+    squareExpression2 = number4 * number4;
+    sumExpression = squareExpression1 + squareExpression2;
+
+    return sumSquareExpression = sumExpression * sumExpression;
+};
+
+calculationExpression(5, 8);
+console.log(sumSquareExpression);
+
+// arrow function
+
+const calculationArrow = (number5, number6) => {
+    squareArrow1 = number5 * number5;
+    squareArrow2 = number6 * number6;
+    sumArrow = squareArrow1 + squareArrow2;
+
+    return sumSquareArrow = sumArrow * sumArrow;
+};
+
+calculationArrow(5, 8);
+console.log(sumSquareArrow);
